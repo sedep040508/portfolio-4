8fbbb6801aa624fb094afbc8739f7c429f8669ab
sedep040508 <elikbaevasedeppka@gmail.com>
2023-11-20 15:44:26 +0600

    added class .js-btn.active:hover

--- js/core.js
+++ js/core.js
@@ -1,7 +1,3 @@
-// let name=prompt("what year are you")
-// alert("i was in"+ name)
-// let age = prompt('Сколько тебе лет?');
-// alert(`Тебе ${15} лет!`);
-document.querySelector('.js-btn').addEventListener('click', function(){
-    alert('btn is pressed')
+document.querySelector('.js-btn').addEventListener('click',function(){
+    document.querySelector('.js-btn').classList.add("active")
 })
