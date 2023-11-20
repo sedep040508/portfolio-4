47827bb5dc1cb66a695b3aaa229b4d7aa937dace
sedep040508 <elikbaevasedeppka@gmail.com>
2023-11-20 21:17:10 +0600

    deleted newhr

--- js/core.js
+++ js/core.js
@@ -1,3 +1,8 @@
 document.querySelector('.js-btn').addEventListener('click',function(){
     document.querySelector('.js-btn').classList.add("active")
+    document.querySelector('.js-btn').style.height="200px"
+    document.querySelector('.js-btn').textContent="tejkdnfkj"
+    var newhr = document.createElement('<p>Paragraph</p>');
+    document.querySelector('.js-btn').insertAdjacentElement('afterend')
+    // document.querySelector('.js-btn').insertAdjacentElement('afterend',"p")
 })
