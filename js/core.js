0bae750b07d5e03465dcd6c4f06ee6aafa3f8245
sedep040508 <elikbaevasedeppka@gmail.com>
2023-11-20 15:29:08 +0600

    added <script src=".//js/core.js"></script>

--- js/core.js
+++ js/core.js
@@ -1,4 +1,7 @@
-let name=prompt("what year are you")
-alert("i was in"+ name)
-let age = prompt('Сколько тебе лет?');
-alert(`Тебе ${15} лет!`);
+// let name=prompt("what year are you")
+// alert("i was in"+ name)
+// let age = prompt('Сколько тебе лет?');
+// alert(`Тебе ${15} лет!`);
+document.querySelector('.js-btn').addEventListener('click', function(){
+    alert('btn is pressed')
+})
